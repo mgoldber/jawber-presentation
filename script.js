090466a81c1344c374ab9cdb42845db55b138200
Shangni <shangnihu@gmail.com>
2019-02-28 11:31:30 -0500

    Added functions section notes

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 
 const meowFunction = function() {
     let hey = 8;
-    return hey;
+
+    let meow = ["peaas", "cars"];
+
 }
 
 console.log("yes");
+var someArray = [1, 4, 6, 2, 17];
+someArray.forEach(/* pass in function here*/);
