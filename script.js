52b221a38f9d57ae66df8f7dabb63e5e9fedff1b
Shangni <shangnihu@gmail.com>
2019-03-11 13:42:34 -0400

    Switched Remote

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ var someArray = [1, 4, 6, 2, 17];
 someArray.forEach(/* pass in function here*/);
 
 document.querySelector();
+
+keysArray.forEach(function (key) {
+    key.addEventListener("transitionend", function (e) {
+        var key = e.target;
+        key.classList.remove("playing");
+    });
+});
